d6a9c53d33c174cc9d829e6de487f87f2b6e5681
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-19 17:56:32 +0800

    增加webapp的功能： 1. 从hdfs的output中读结果数据在网页中显示

--- src/web_app.py
+++ src/web_app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from hdfs import InsecureClient
+import codecs
+import json
+
 
 app = Flask(__name__)
 
 
 @app.route("/<sentence>")
 def hello(sentence):
-    return repr(sentence)
+    hdfs_client = InsecureClient('http://127.0.0.1:50070')
+    nearest_posts = None
+    with hdfs_client.read('output_1730/part-00000') as reader:
+        for line in reader:
+            nearest_posts = codecs.decode(line.strip(), "utf-8")
+    return nearest_posts
 app.run()
