8e4d8aa3d964fcd361efe3512928b2eaf7b9855f
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-25 21:04:40 +0800

    refactor<src>: 将mapreduce程序都放在hadoop目录下

--- src/nearest_items/hadoop/mapper.py
+++ src/nearest_items/hadoop/mapper.py
@@ -2,19 +2,18 @@
 # coding=utf-8
 
 import sys
-import jieba
 
 from hdfs import InsecureClient
-from base import similarity
+from src.nearest_items.hadoop.base import similarity
 
 # 将"我的收藏"里的微博放入一个list中
 hdfs_client = InsecureClient('http://127.0.0.1:50070')
 my_favorites = list()
 with hdfs_client.read('my_favorites.json') as reader:
     for line in reader:
-        my_favorites.extend([w for w in jieba.cut(line, cut_all=True)])
+        my_favorites.extend(line.strip().split(" "))
 
 for line in sys.stdin:
     line = line.strip("\n")
-    one_post = [w for w in jieba.cut(line, cut_all=True)]
+    one_post = line.strip().split(" ")
     print repr(similarity(one_post, my_favorites) / len(my_favorites)) + "," + line
--- src/nearest_items/hadoop/run_mr.py
+++ src/nearest_items/hadoop/run_mr.py
@@ -4,7 +4,7 @@ import subprocess
 from hdfs import InsecureClient
 
 client = InsecureClient("http://127.0.0.1:50070")
-client.delete("best_posts")
+
 cmd = ["hadoop", "jar", "/Users/wumengling/src/pig-0.15.0/test/e2e/pig/lib/hadoop-streaming.jar",
        "-file", "./mapper.py",
        "-mapper", "./mapper.py",
